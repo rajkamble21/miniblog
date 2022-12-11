58d0349cbb80faa8e03c3ad3674768d664503699
Raj Kamble <rajskamble216@gmail.com>
2022-12-11 22:15:42 +0530

    media root added

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = "/var/www/example.com/static/"
-MEDIA_ROOT = BASE_DIR
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
